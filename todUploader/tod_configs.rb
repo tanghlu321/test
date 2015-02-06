b8a6e4a8266643ae39819326b7ecf857a4f3cb23
Kai Wang <kaiwang@groupon.com>
2015-02-06 15:25:27 -0800

    Updating TOD ymls cr=sparta, checkin file:

--- todUploader/tod_configs.rb
+++ todUploader/tod_configs.rb
@@ -16,8 +16,8 @@ permalink_string = "##permalink##"
 
 updates = CSV.read(ARGV[0])
 config_dir='/Users/kaiwang/Project/test/batch_intl/'
+print config_dir
 for config in Dir.glob(config_dir +  "*.yml")
-  print config
   config_file = YAML.load_file(config)
   tod_overrides = config_file['todOverrides']
   updated = false
