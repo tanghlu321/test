7d23daca44bf77549add4707b8db5642dd6e7fd2
Kai Wang <kaiwang@groupon.com>
2015-02-10 15:37:03 -0800

    Updating TOD ymls cr=sparta

--- todUploader/server/api/upload/upload.controller.js
+++ todUploader/server/api/upload/upload.controller.js
@@ -11,70 +11,92 @@ exports.index = function(req, res) {
 };
 
 var printLogs = function(message, err, stdout, stderr){
-  console.log(message + err);
+  console.log(message + JSON.stringify(err));
   console.log('stdout: ' + stdout);
   console.log('stderr: ' + stderr);
 }
 
-var checkin = function(fileName, callback){
-  exec('cd /Users/kaiwang/Projects/test && git add .', function (err, stdout, stderr){
-    printLogs('git add error:', err, stdout, stderr);
-    if (err !== null){
-      return callback(err);
-    } 
+// var checkin = function(fileName, callback){
+//   exec('cd /Users/kaiwang/Projects/test && git add .', function (err, stdout, stderr){
+//     printLogs('git add error:', err, stdout, stderr);
+//     if (err !== null){
+//       return callback(err);
+//     } 
 
-    // include the source CVS file name and the user who is making the upload
-    // this will help diagnoze issues with a given checkin
-    exec('git commit -m "Updating TOD ymls cr=sparta"', function (err, stdout, stderr){
-      printLogs('git commit error:', err, stdout, stderr); 
+//     exec('git commit -m "Updating TOD ymls cr=sparta"', function (err, stdout, stderr){
+//       printLogs('git commit error:', err, stdout, stderr); 
       
-      exec('git push origin master', function (err, stdout, stderr){
-        printLogs('git push error:', err, stdout, stderr);
-        if (err !== null){
-          return callback(err);
-        } 
-      });
-    });
-    return callback(null);
-  });
-};
+//       exec('git push origin master', function (err, stdout, stderr){
+//         printLogs('git push error:', err, stdout, stderr);
+//         if (err !== null){
+//           return callback(err);
+//         } 
+//       });
+//     });
+//     return callback(null);
+//   });
+// };
 
+var afterPullMaster = function(err, stdout, stderr, file, res){
+  printLogs('git pull error:', err, stdout, stderr);
+  if (err !== null){
+    res.status(500).send({Error: "Git pull error " + err.message});
+    return next();
+  }
+  exec('ruby tod_configs.rb ' + file.path, function(err, stdout, stderr){
+    afterExecRuby(err, stdout, stderr, res);
+  });
+}
+
+var afterExecRuby = function(err, stdout, stderr, res){
+  printLogs('update .yaml files error:', err, stdout, stderr);
+  if (err !== null){
+    res.status(500).send({Error: "when updating .yml files " + err.message});
+    return next();
+  } 
+  exec('cd /Users/kaiwang/Projects/test && git add .', function(err, stdout, stderr){
+    afterGitAdd(err, stdout, stderr, res);
+  });
+}
+
+var afterGitAdd = function(err, stdout, stderr, res){
+  printLogs('git add error:', err, stdout, stderr);
+  if (err !== null){
+    res.status(500).send({Error: "Git add error " + err.message});
+    return next();
+  } 
+  exec('git commit -m "Updating TOD ymls cr=sparta"', function(err, stdout, stderr){
+    afterGitCommit(err, stdout, stderr, res);
+  });
+}
+
+var afterGitCommit = function(err, stdout,stderr, res){
+  printLogs('git commit error:', err, stdout, stderr); 
+  if (err !== null){
+    res.status(500).send({Error: "Git commit error " + err.message});
+    return next();
+  } 
+  exec('git push origin master', function(err, stdout, stderr){
+    afterGitPush(err, stdout, stderr);
+  });
+}
+
+function afterGitPush(err, stdout, stderr, res){
+  printLogs('git push error:', err, stdout, stderr);
+}
 
 exports.create = function (req, res, next) {
+  var data = _.pick(req.body, 'type')
+    , uploadPath = path.normalize('./uploads')
+    , file = req.files.file;
 
-  var tryCount = 0;
-  var update = function(){
-    exec('cd /Users/kaiwang/Projects/test && git pull origin master', function (err, stdout, stderr) {
-      printLogs('git pull error:', err, stdout, stderr);
-      if (err !== null){
-        res.status(500).send({Error: "Git pull error " + err.message});
-        return next();
-      } 
-    
-      exec('ruby tod_configs.rb ' + file.path, function (err, stdout, stderr) {
-        printLogs('update .yaml files error:', err, stdout, stderr);
-        if (err !== null){
-          res.status(400).send({Error: "when updating .yml files " + err.message});
-          return next();
-        } 
-        
-        checkin (file.name, function (err){
-          if (err !== null){
-            if (tryCount > 5){
-              res.status(500).send({Error: "Git checkin error: " + err});
-              return next();
-            }
-    
-            tryCount++;
-            exec('git reset --hard HEAD~1', function (err, stdout, stderr){
-              update();
-            });
-          }
-          else 
-            res.status(200).end();
-        });
-      });
+  exec('cd /Users/kaiwang/Projects/test && git pull origin master', function(err, stdout, stderr){
+    afterPullMaster(err, stdout, stderr, file, res, function(err){
+      if (err == null){
+        res.status(200).end();
+      }
+      else 
+        res.status(500).send({Error: err.message});
     });
-  };
-  update();
+  });
 };
