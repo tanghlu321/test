f33b76d552def74a9236eb466bad763ea5d7f063
Kai Wang <kaiwang@groupon.com>
2015-02-11 10:54:52 -0800

    Updating TOD ymls cr=sparta

--- todUploader/client/app/main/main.controller.js
+++ todUploader/client/app/main/main.controller.js
@@ -22,7 +22,7 @@ angular.module('todUploaderApp')
           console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
         }).success(function(data, status, headers, config) {
           if (status == 200) {
-            $scope.message = "Success! Please check the updates on Github";
+            $scope.message = "Success! Please check the updates on Github for safety";
           }
           // file is uploaded successfully
           console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
