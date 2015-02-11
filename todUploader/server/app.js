1af8808d5011965d966278db46d374be97d04427
Kai Wang <kaiwang@groupon.com>
2015-02-11 11:47:29 -0800

    Updating TOD ymls cr=sparta

--- todUploader/server/app.js
+++ todUploader/server/app.js
@@ -5,7 +5,9 @@
 'use strict';
 
 // Set default node environment to development
+console.log(process.env.NODE_ENV);
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log(process.env.NODE_ENV);
 
 var express = require('express');
 var multipart = require('connect-multiparty');
